4c29ffc636ddf04cbc69b56dd5bcbbff28ac4913
jinki <jkhsky121@gmail.com>
2023-12-31 13:25:16 +0900

    setup login router

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,3 +1,20 @@
 import React from "react";
+import { Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Login } from "../pages/login";
+import { CreateAccount } from "../pages/create-account";
 
-export const LoggedOutRouter = () => <span>Logged Out</span>;
+export const LoggedOutRouter = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/create-account">
+          <CreateAccount />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
